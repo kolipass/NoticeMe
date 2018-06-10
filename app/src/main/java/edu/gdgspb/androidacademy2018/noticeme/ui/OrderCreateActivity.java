14469e245f4f8fdf719519041b8281a5476cc931
Aleksandr Shargun <shargyn@ya.ru>
2018-06-11 01:12:39 +0300

    Coreect working app

--- app/src/main/java/edu/gdgspb/androidacademy2018/noticeme/ui/OrderCreateActivity.java
+++ app/src/main/java/edu/gdgspb/androidacademy2018/noticeme/ui/OrderCreateActivity.java
@@ -12,34 +12,61 @@ import android.widget.Toast;
 
 import com.facebook.stetho.Stetho;
 
+import java.util.Calendar;
+import java.util.Date;
+
 import edu.gdgspb.androidacademy2018.noticeme.CheckLocationService;
 import edu.gdgspb.androidacademy2018.noticeme.R;
+import edu.gdgspb.androidacademy2018.noticeme.Utils;
 import edu.gdgspb.androidacademy2018.noticeme.db.AppDatabase;
 import edu.gdgspb.androidacademy2018.noticeme.db.Note;
+import edu.gdgspb.androidacademy2018.noticeme.ui.map.PointSelectorActivity;
+import edu.gdgspb.androidacademy2018.noticeme.ui.orderlist.OrderListActivity;
 
 public class OrderCreateActivity extends Activity {
+    private double latitude;
+    private double longitude;
+    private double radius;
     private EditText title;
     private EditText dateOfCreate;
     private EditText dateOfLife;
     private EditText dateOfRun;
-    private EditText longitude;
-    private EditText latitude;
+    private EditText longitudeEt;
+    private EditText latitudeEt;
     private CheckBox iscanceled;
-
+    private Date currentTime;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+        Stetho.initializeWithDefaults(this);
+        getDataFromIntent();
+        currentTime = Calendar.getInstance().getTime();
+        setUpView();
+    }
+
+    private void setUpView() {
         title = findViewById(R.id.editText_Name);
         dateOfCreate = findViewById(R.id.editText_date_create);
+        String dateCreate = Utils.getStringByDate(currentTime);
+        dateOfCreate.setText(dateCreate);
         dateOfLife = findViewById(R.id.editText_date_life);
         dateOfRun = findViewById(R.id.editText_date_run);
-        longitude = findViewById(R.id.editText_longitude);
-        latitude = findViewById(R.id.editText_latitude);
+        longitudeEt = findViewById(R.id.editText_longitude);
+        longitudeEt.setText(String.valueOf(longitude));
+        latitudeEt = findViewById(R.id.editText_latitude);
+        latitudeEt.setText(String.valueOf(latitude));
+
         iscanceled = findViewById(R.id.canceled);
         Stetho.initializeWithDefaults(this);
     }
 
+    private void getDataFromIntent() {
+            Intent intent = getIntent();
+            latitude = intent.getExtras().getDouble(PointSelectorActivity.LATITUDE);
+            longitude = intent.getExtras().getDouble(PointSelectorActivity.LONGITUDE);
+            radius = intent.getExtras().getInt(PointSelectorActivity.RADIUS);
+    }
 
     public void onClick(View view) {
         //Добавим строку в БД
@@ -51,18 +78,19 @@ public class OrderCreateActivity extends Activity {
         startService(new Intent(this, CheckLocationService.class));
     }
 
-    //******************************************************************
     private void onAddNoteBtnClick() {
         Note note = createNote(title.getText().toString(),
-                Long.parseLong(dateOfCreate.getText().toString()),
+                currentTime.getTime(),
                 Long.parseLong(dateOfLife.getText().toString()),
                 Long.parseLong(dateOfRun.getText().toString()),
                 iscanceled.isChecked(),
-                Double.parseDouble(latitude.getText().toString()),
-                Double.parseDouble(longitude.getText().toString()));
+                Double.parseDouble(latitudeEt.getText().toString()),
+                Double.parseDouble(longitudeEt.getText().toString()));
 
         AppDatabase.getInstance(this).noteDao().insert(note);
-
+        Intent intent = new Intent(this, OrderListActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 
     @NonNull
@@ -79,6 +107,4 @@ public class OrderCreateActivity extends Activity {
         Log.d("myTag", "createNote: ");
         return note;
     }
-
-    //******************************************************************
 }
---
+++ app/src/main/java/edu/gdgspb/androidacademy2018/noticeme/ui/map/PointSelectorActivity.java
@@ -0,0 +1,160 @@
+package edu.gdgspb.androidacademy2018.noticeme.ui.map;
+
+import android.Manifest;
+import android.app.Dialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
+import android.view.View;
+import android.widget.Button;
+import android.widget.NumberPicker;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import java.util.Locale;
+import edu.gdgspb.androidacademy2018.noticeme.R;
+import edu.gdgspb.androidacademy2018.noticeme.ui.OrderCreateActivity;
+
+public class PointSelectorActivity extends FragmentActivity implements OnMapReadyCallback {
+    public static final String LATITUDE = "latitude";
+    public static final String LONGITUDE = "longitude";
+    public static final String RADIUS = "radius";
+    private GoogleMap mMap;
+    public static final int REQUEST_LOCATION_PERMISSION = 1;
+    public int radius = 0;
+    private double latitude;
+    private double longitude;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_point_selector);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        LatLng spb = new LatLng(60, 30);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(spb));
+        setMapLongClick(mMap);
+        enableMyLocation();
+
+        mMap.setOnCircleClickListener(new GoogleMap.OnCircleClickListener() {
+
+            @Override
+            public void onCircleClick(Circle circle) {
+                openOrderCreateActivity();
+            }
+        });
+    }
+
+    private void setMapLongClick(final GoogleMap map) {
+        map.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+            @Override
+            public void onMapLongClick(LatLng latLng) {
+                latitude = latLng.latitude;
+                longitude = latLng.longitude;
+                String snippet = String.format(Locale.getDefault(),
+                        "Lat: %1$.5f, Long: %2$.5f",
+                        latitude,
+                        longitude);
+
+                map.addMarker(new MarkerOptions()
+                        .position(latLng)
+                        .title(getString(R.string.dropped_pin))
+                        .snippet(snippet));
+                showDialog();
+            }
+        });
+    }
+
+    private void drawCircle(double latitude, double longitude, GoogleMap map) {
+        Circle circle = map.addCircle(new CircleOptions()
+                .center(new LatLng(latitude, longitude))
+                .clickable(true)
+                .radius(radius)
+                .strokeWidth(3)
+                .strokeColor(Color.RED)
+                .fillColor(0x550000FF));
+    }
+
+    private void enableMyLocation() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]
+                            {Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_LOCATION_PERMISSION);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_LOCATION_PERMISSION:
+                if (grantResults.length > 0
+                        && grantResults[0]
+                        == PackageManager.PERMISSION_GRANTED) {
+                    enableMyLocation();
+                    break;
+                }
+        }
+    }
+
+    public void showDialog() {
+        final Dialog dialog = new Dialog(this);
+        dialog.setTitle("Radius");
+        dialog.setContentView(R.layout.dialog);
+        Button set = dialog.findViewById(R.id.set);
+        Button cancel = dialog.findViewById(R.id.cancel);
+        final NumberPicker numberPicker = dialog.findViewById(R.id.numberPicker);
+        numberPicker.setMaxValue(50); // max value
+        numberPicker.setMinValue(1);   // min value
+        numberPicker.setWrapSelectorWheel(false);
+
+        set.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numberPicker.clearFocus();
+                radius = numberPicker.getValue() * 1000;
+                dialog.dismiss();
+                drawCircle(latitude,longitude, mMap);
+            }
+        });
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mMap.clear();
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
+    }
+
+    public void openOrderCreateActivity() {
+        Intent intent = new Intent(this, OrderCreateActivity.class);
+        intent.putExtra(LATITUDE, latitude);
+        intent.putExtra(LONGITUDE, longitude);
+        intent.putExtra(RADIUS, radius);
+        startActivity(intent);
+    }
+}
